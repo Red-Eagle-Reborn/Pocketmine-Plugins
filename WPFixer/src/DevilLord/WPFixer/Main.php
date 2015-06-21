9afc37e8c692c87a409340b00381693ee88f53a4
DevillordMCPE <maulaxe@yahoo.com>
2015-06-21 14:24:15 +0700

    Create Main.php

---
+++ WPFixer/src/DevilLord/WPFixer/Main.php
@@ -0,0 +1,23 @@
+<?php
+namespace PixelGuy75\JoinCommands;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener {
+  public function onEnable() {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->saveDefaultConfig();
+  }
+  public function onJoin(PlayerJoinEvent $event) {
+    if($this->getConfig()->get("enableCommand") == "true"){
+      $player = $event->getPlayer();
+      foreach($this->getConfig()->get("Command") as $command){
+        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{player}", $player->getName(), $command));	
+      }
+    }
+  }
+}
+?>
